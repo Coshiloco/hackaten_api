34ea4f1cc4dee7e5410101a487273ccf6930e2a2
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2022-10-01 10:46:33 +0200

    Modelos Tabla intermedia terminados

--- hackapi/db/models.py
+++ hackapi/db/models.py
@@ -1,3 +1,5 @@
+from enum import unique
+
 from hackapi.db.database import Base
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
@@ -5,10 +7,11 @@ from sqlalchemy.orm import relationship
 
 class Habitacion(Base):
     __tablename__ = 'Habitacion'
-    id = Column(Integer, autoincrement = True)
+    id = Column(Integer, unique = True,  autoincrement = True)
     Nombre = Column(String, primary_key = True)
+    Codigo_De_Habitacion = Column(String, unique = True)
     Bombillas = [relationship('Bombilla', backref = 'Habitacion', cascade = 'delete,merge')]
-
+    UserBombillasHabitaciones = [relationship('UserBombillasHabitaciones', backref = 'Habitacion', cascade = 'delete,merge')]
 
 class Bombilla(Base):
     __tablename__ = 'Bombilla'
@@ -17,5 +20,23 @@ class Bombilla(Base):
     Color = Column(String)
     Brillo = Column(Integer)
     Encendido_Apagado = Column(Boolean)
+    Codigo_De_Bombilla = Column(String, unique = True)
+    UserBombillasHabitaciones = [relationship('UserBombillasHabitaciones', backref = 'Bombilla', cascade = 'delete,merge')]
     Habitacion_Nombre= Column(String,  ForeignKey("Habitacion.Nombre", ondelete = 'CASCADE'))   
 
+class User(Base):
+    __tablename__ = "Users"
+    id = Column(Integer, primary_key = True, autoincrement = True)
+    User_Name = Column(String)
+    Email = Column(String)
+    Password = Column(String)
+    Cantidad_de_Bombillas = Column(Integer)
+    Cantidad_de_Habitaciones  = Column(Integer)
+    UserBombillasHabitaciones = [relationship('UserBombillasHabitaciones', backref = 'Users', cascade = 'delete,merge')]
+
+class UserBombillasHabitaciones(Base):
+    __tablename__ = "UserBombillasHabitaciones"
+    id = Column(Integer, primary_key = True, autoincrement = True)
+    Usuario_Id= Column(Integer,  ForeignKey("Users.id", ondelete = 'CASCADE'))   
+    Codigo_Habitacion= Column(String,  ForeignKey("Habitacion.Codigo_De_Habitacion", ondelete = 'CASCADE'))   
+    Codigo_Bombilla= Column(String,  ForeignKey("Bombilla.Codigo_De_Bombilla", ondelete = 'CASCADE'))   
